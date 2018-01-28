e8ce7a4e6aa43a750f25ede96e7c9c022832c011
Amanksingh16 <amansingh16597@gmail.com>
2018-01-27 17:44:23 -0800

    Runs success for xyz

--- Substrings_Strings.java
+++ Substrings_Strings.java
@@ -7,14 +7,20 @@ public class Substrings_Strings {
 		System.out.println("Enter a String !");
 		String a = sc.nextLine();
 		String b = new String();
+		String c = new String();
 		for(int i=0; i<a.length() ; i++)
 		{
 			for(int j=i+1; j<=a.length(); j++)
 			{
+				if((i==0)&&(j==a.length()-(i+1)))
+				{
+				   c = (a.charAt(i))+""+(a.charAt(j));	
+					System.out.print(c+" ");
+				}
 				b = a.substring(i,j);	
-				System.out.print(b+" ");				
+				System.out.print(b+" ");					
 			}
-			
+
 		}
         
 	}
