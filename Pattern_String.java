b54aae1dceb09657d064f074c454190cc5f384f0
Amanksingh16 <amansingh16597@gmail.com>
2018-01-27 17:07:49 -0800

    Run Success

---
+++ Pattern_String.java
@@ -0,0 +1,17 @@
+
+public class Pattern_String {
+
+	public static void main(String[] args) 
+	{
+		for(int i='a'; i<'d'; i++)
+		{
+			for(int j='a';j<=i;j++)
+			{
+				System.out.print((char)(j));
+			}
+			System.out.println();
+		}
+
+	}
+}
+
